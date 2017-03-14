01977f1c9b6e9afe7da586119f226ccf68ee00b4
Ben <benpiggott@PIGGOTT.EU>
2017-03-14 15:44:53 +0000

    Data Validation
    Added data validation for addint and modifying users however the
    behaviour is odd at times.

--- resources/user.js
+++ resources/user.js
@@ -156,12 +156,14 @@ function loadModalVariables() {
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
+        document.getElementById("userAdd").reset();
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
+            document.getElementById("userAdd").reset();
         }
     }
 }
